8f44fd904a5f81bd93d1c7c622d9328036426468
arshinanu <hdarshin@gmail.com>
2022-01-09 11:12:28 +0000

    fourth commit

--- src/test/java/com/arshin/jenkins/dummy/DummyApplicationTests.java
+++ src/test/java/com/arshin/jenkins/dummy/DummyApplicationTests.java
@@ -10,7 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class DummyApplicationTests {
+public class DummyApplicationTests {
 	public static Logger logger=LoggerFactory.getLogger(DummyApplicationTests.class);
 
 	@Test
